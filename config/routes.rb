4883d5266e489b4ca0cbb184a43d4a7f268bdedc
talk2camden <scoobydooismylife@gmail.com>
2017-06-22 17:37:47 +0000

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
 
   get 'about-me', to: 'pages#about'
 
   get 'contact', to: 'pages#contact'
+  
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
