72e5601857b5d1f57a29ecc258e83c91189d11ed
talk2camden <scoobydooismylife@gmail.com>
2017-06-26 16:17:22 +0000

    Added toggle button for status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -56,6 +56,16 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+  if @blog.draft?
+    @blog.published!
+  else if @blog.published?
+    @blog.draft!
+    end
+  end
+  redirect_to blogs_url, notice: 'Post status has been updated.'
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  resources :blogs
+  resources :blogs do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    member do
+      get :toggle_status
+    end
+  end
   root to: 'pages#home'
 end
