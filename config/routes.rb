f907173574362f656521f0a5e5091fe8eb08321b
talk2camden <scoobydooismylife@gmail.com>
2017-06-29 15:27:01 +0000

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
-
   get 'contact', to: 'pages#contact'
   
-  get 'angular-items', to: 'portfolios#angular'
- 
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   resources :blogs do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
