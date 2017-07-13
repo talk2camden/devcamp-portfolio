de5b4c955723bb2f03e85672891ee996a7ccf75c
talk2camden <scoobydooismylife@gmail.com>
2017-07-13 17:20:57 +0000

    Integrated new gem into rails for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+  CamdenBoydViewTool::Renderer.copyright 'Camden Boyd', 'All rights reserved'
+end
 end
