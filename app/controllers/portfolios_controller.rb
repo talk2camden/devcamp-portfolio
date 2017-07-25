bf4a7fc4f03ba553e41e3bab92af1ebc381fbc5a
talk2camden <scoobydooismylife@gmail.com>
2017-07-25 15:54:53 +0000

    Updated portfolio to include attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,70 +4,70 @@ class PortfoliosController < ApplicationController
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
-def angular
-  @angular_portfolio_items = Portfolio.angular
-end
-
-def new
-  @portfolio_item = Portfolio.new
-  3.times { @portfolio_item.technologies.build }
-end
-
-def create
-    @portfolio_item = Portfolio.new(portfolio_params)
-
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Forsooth! It appears that portfolio item was successfully created.' }
-      else
-        format.html { render :new }
-      end
-    end
-end
-
-def edit
-end
-
-def update
-    respond_to do |format|
-      if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
-    end
-end
-
-def show
-end
-
-def destroy
-  # Perform the lookup
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
   
-  # Destroy/delete the record
-  @portfolio_item.destroy
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+  
+  def create
+      @portfolio_item = Portfolio.new(portfolio_params)
+  
+      respond_to do |format|
+        if @portfolio_item.save
+          format.html { redirect_to portfolios_path, notice: 'Forsooth! It appears that portfolio item was successfully created.' }
+        else
+          format.html { render :new }
+        end
+      end
+  end
+  
+  def edit
+  end
+  
+  def update
+      respond_to do |format|
+        if @portfolio_item.update(portfolio_params)
+          format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
+     end
+  end
+  
+  def show
+  end
+  
+  def destroy
+    # Perform the lookup
     
-    # Redirect
-    respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Someone has died, and so has this record.' }
-    end
-end
-
-private
-
-def portfolio_params
-  params.require(:portfolio).permit(:title, 
-                                    :subtitle, 
-                                    :body, 
-                                    technologies_attributes: [:name]
-                                    )
-end
-
-def set_portfolio_item
-  @portfolio_item = Portfolio.find(params[:id])
-end
+    # Destroy/delete the record
+    @portfolio_item.destroy
+      
+      # Redirect
+      respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: 'Someone has died, and so has this record.' }
+      end
+  end
+  
+  private
+  
+  def portfolio_params
+    params.require(:portfolio).permit(:title, 
+                                      :subtitle, 
+                                      :body, 
+                                      technologies_attributes: [:name]
+                                      )
+  end
+  
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
 
